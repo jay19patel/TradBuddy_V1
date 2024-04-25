2c1964448eda5ad0705a33f43b88cd3179a2ffea
JAY PATEL <107461719+jay19patel@users.noreply.github.com>
2024-04-25 14:08:57 +0530

    Update new

--- Algo/Trader/test.py
+++ Algo/Trader/test.py
@@ -1,49 +1,38 @@
 import concurrent.futures
-from PlaceOrderManager import PlaceOrder
 
 strategies_results = {
     "NSE:NIFTY50-INDEX": {"strategy_1_status": "CE", "strategy_2_status": "PE", "updated_datetime": "2024-04-20 22:43:42"},
     "NSE:NIFTYBANK-INDEX": {"strategy_1_status": "CE", "strategy_2_status": "PE", "updated_datetime": "2024-04-20 22:43:42"}
 }
 
-Account_list = {
-    "111": {
-        "Strategy_1": ["NSE:NIFTY50-INDEX"],
-        "Strategy_2": ["NSE:NIFTYBANK-INDEX"]
+Account_list = [
+    {
+        "name": 111,
+        "strategy": [
+            {"NSE:NIFTY50-INDEX": "strategy_1_status"},
+            {"NSE:NIFTYBANK-INDEX": "strategy_1_status"}
+            ]
     },
-    "222": {
-        "Strategy_1": ["NSE:NIFTY50-INDEX"],
-        "Strategy_2": ["NSE:NIFTYBANK-INDEX", "NSE:NIFTYBANK-INDEX", "NSE:NIFTYBANK-INDEX", "NSE:NIFTYBANK-INDEX"]
-    },
-    "333": {
-        "Strategy_1": ["NSE:NIFTY50-INDEX"],
-        "Strategy_2": ["NSE:NIFTYBANK-INDEX", "NSE:NIFTYBANK-INDEX", "NSE:NIFTYBANK-INDEX", "NSE:NIFTYBANK-INDEX"]
-    },
-    "444": {
-        "Strategy_1": ["NSE:NIFTY50-INDEX"],
-        "Strategy_2": ["NSE:NIFTYBANK-INDEX", "NSE:NIFTYBANK-INDEX", "NSE:NIFTYBANK-INDEX", "NSE:NIFTYBANK-INDEX"]
+    {
+        "name": 222,
+        "strategy": [
+            {"NSE:NIFTY50-INDEX": "strategy_2_status"},
+            {"NSE:NIFTYBANK-INDEX": "strategy_2_status"}
+            ]
     }
-}
+]
 
-def worker(account_id, strategy_name, symbol, status):
-    PlaceOrder(account_id, strategy_name, symbol, status)
+def StrategyCode(args):
+    name, strategy = args
+    print("Account Name :",name)
+    for i in strategy:
+      (symbol, strategy) = next(iter(i.items()))
+      print(strategies_results[symbol][strategy])
+      print(symbol,strategy)
 
-def coin_current_check(executor, Account_list, strategies_results):
-    futures = []
-    for account_id, strategies in Account_list.items():
-        for strategy_name, symbols in strategies.items():
-            for symbol in symbols:
-                if symbol in strategies_results:
-                    result = strategies_results[symbol]
-                    status = result[f'{strategy_name.lower()}_status']
-                    futures.append(executor.submit(worker, account_id, strategy_name, symbol, status))
-    for future in concurrent.futures.as_completed(futures):
-        future.result()
+def process_item(n):
+    with concurrent.futures.ThreadPoolExecutor() as executor1:
+        executor1.map(StrategyCode, [(n['name'], n['strategy'])])
 
-def main():
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        coin_current_check(executor, Account_list, strategies_results)
-
-
-if __name__ == "__main__":
-    main()
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    executor.map(process_item, Account_list)
