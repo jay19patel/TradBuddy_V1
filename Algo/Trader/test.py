f2f54df84647ce2e99a67e84fdf1f8ead9f96f22
JAY PATEL <107461719+jay19patel@users.noreply.github.com>
2024-04-27 17:28:32 +0530

    Final Change

--- Algo/Trader/test.py
+++ Algo/Trader/test.py
@@ -1,4 +1,3 @@
-import time
 import asyncio
 
 strategies_results = {
@@ -6,32 +5,34 @@ strategies_results = {
     "NSE:NIFTYBANK-INDEX": {"strategy_1_status": "CE", "strategy_2_status": "PE", "updated_datetime": "2024-04-20 22:43:42"}
 }
 
-account_strategies = {
-    111: [("NSE:NIFTY50-INDEX", "strategy_1_status"), ("NSE:NIFTYBANK-INDEX", "strategy_1_status")],
-    222: [("NSE:NIFTY50-INDEX", "strategy_2_status"), ("NSE:NIFTYBANK-INDEX", "strategy_2_status")]
-}
-def Time_set_for_next_day(market_status):
-    schedule.clear()
-    schedule.every().day.at("09:15").do(Worker)
-    print(f"Market Status is : {market_status}")
+accounts = [
+    {
+        "account": 111,
+        "strategies": [("NSE:NIFTY50-INDEX", "strategy_1_status"), ("NSE:NIFTYBANK-INDEX", "strategy_1_status")]
+    },
+    {
+        "account": 222,
+        "strategies": [("NSE:NIFTY50-INDEX", "strategy_2_status"), ("NSE:NIFTYBANK-INDEX", "strategy_1_status")]
+    }
+]
 
-async def strategy_code(account_id, strategies):
-    for symbol, strategy in strategies:
-        status = strategies_results[symbol][strategy]
-        # Process status, e.g., await some_async_function(status)
-        # await asyncio.sleep(3)  # Replace this with your actual processing
-        print(symbol, strategy,status)
 
-async def process_account(account_id, strategies):
-    await strategy_code(account_id, strategies)
+async def strategy_run(account, symbol, status):
+    await asyncio.sleep(2)  # Wait for 2 seconds
+    print(account, symbol, status)
+
+
+async def process_account(account):
+    account_number = account["account"]
+    tasks = []
+    for symbol, strategy_key in account["strategies"]:
+        status = strategies_results.get(symbol, {}).get(strategy_key)
+        tasks.append(strategy_run(account_number, symbol, status))
+    await asyncio.gather(*tasks)
+
 
 async def main():
-    start_time = time.time()
-    tasks = [process_account(account_id, strategies) for account_id, strategies in account_strategies.items()]
-    await asyncio.gather(*tasks)
-    end_time = time.time()
-    execution_time = end_time - start_time
-    print(f"Execution Time: {execution_time} seconds")
+    await asyncio.gather(*(process_account(account) for account in accounts))
 
-# Run the async function
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
